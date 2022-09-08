48e05c9c91bfcd0aa8a256e20e7c6141e9c27aa3
DavidOchido <davidochido1@gmail.com>
2022-09-08 15:40:50 -0700

    mine

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+char c;
+int i;
+long li;
+long long lli;
+float f;
+
+printf("Size of char: %ld byte(s)\n", sizeof(c));
+return (0);
+}
